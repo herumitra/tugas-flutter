6ff08dfdc2830a73dae7bcf889a7cfd456b65420
Heru Oktafian <heru@Herus-Mac-mini.local>
2025-01-23 23:42:10 +0700

    Add tugas week 01 Kondisional & Perulangan

---
+++ tugas_week_01_kondisi_dan_perulangan.dart
@@ -0,0 +1,10 @@
+void main() {
+  // Perulangan dari 1 sampai 20
+  for (int i = 1; i <= 20; i++) {
+    // Memeriksa apakah bilangan genap
+    if (i % 2 == 0) {
+      // Menampilkan bilangan genap
+      print(i);
+    }
+  }
+}
